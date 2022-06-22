a9cd0ad5a73da72eee891453c0454ab14a25d53a
Lluis Miralles <luismiralles98@gmail.com>
2022-06-22 12:40:04 +0200

    DontDestroyOnLoad

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -6,8 +6,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : Singleton<MusicManager>
+public class MusicManager : MonoBehaviour
 {
+    public static MusicManager Instance { get; private set; }
 
     [SerializeField] private EventReference _loungeMusicReference;
     [SerializeField] private EventReference _interviewMusicReference;
@@ -20,6 +21,15 @@ public class MusicManager : Singleton<MusicManager>
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Start()
@@ -35,21 +45,17 @@ public class MusicManager : Singleton<MusicManager>
 
     public void PlayLoungeMusic()
     {
-        StopMainMenuMusic();
-
         _loungeMusicInstance = RuntimeManager.CreateInstance(_loungeMusicReference);
         _loungeMusicInstance.start();
     }
 
     public void PlayInterviewMusic()
     {
-        StopLoungeMusic();
-
         _interviewMusicInstance = RuntimeManager.CreateInstance(_interviewMusicReference);
         _interviewMusicInstance.start();
     }
 
-    private void StopMainMenuMusic()
+    public void StopMainMenuMusic()
     {
         PLAYBACK_STATE playbackState;
         _mainMenuInstance.getPlaybackState(out playbackState);
@@ -57,7 +63,7 @@ public class MusicManager : Singleton<MusicManager>
         if (playbackState == PLAYBACK_STATE.PLAYING) _mainMenuInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 
-    private void StopLoungeMusic()
+    public void StopLoungeMusic()
     {
         PLAYBACK_STATE playbackState;
         _loungeMusicInstance.getPlaybackState(out playbackState);
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,24 +6,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : Singleton<SceneLoader>
+public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance { get; private set; }
+
     [SerializeField] private FadeIn _fadeScreen;
 
-    [SerializeField] private Camera _explorationCam;
-    [SerializeField] private Controller _explorationPlayerController;
+    //[SerializeField] private Camera _explorationCam;
+    //[SerializeField] private Controller _explorationPlayerController;
 
-    public void LoadInterviewScene()
+    private void Awake()
     {
-        MusicManager.Instance.PlayInterviewMusic();
-        _fadeScreen.FadeOut(() =>
-        {
-            _explorationCam.gameObject.SetActive(false);
-            _explorationPlayerController.enabled = false;
+        DontDestroyOnLoad(this.gameObject);
 
-            var async = SceneManager.LoadSceneAsync(Scenes.INTERVIEW_SCENE, LoadSceneMode.Additive);
-            async.completed += OnSceneLoaded;
-        });
+        if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void LoadMainMenuScene()
@@ -32,16 +34,45 @@ public class SceneLoader : Singleton<SceneLoader>
         MusicManager.Instance.PlayMainMenuMusic();
         _fadeScreen.FadeOut(() =>
         {
-            _explorationCam.gameObject.SetActive(true);
-            _explorationPlayerController.enabled = true;
-
             var async = SceneManager.LoadSceneAsync(Scenes.MAIN_MENU_SCENE);
+            async.completed += OnSceneLoaded;
+        });
+    }
+
+    public void LoadLoungeScene()
+    {
+        MusicManager.Instance.StopMainMenuMusic();
+        MusicManager.Instance.PlayLoungeMusic();
+        _fadeScreen.FadeOut(() =>
+        {
+            var async = SceneManager.LoadSceneAsync(Scenes.MAIN_MENU_SCENE);
+            async.completed += OnLoungeSceneLoaded;
+        });
+    }
+
+    public void LoadInterviewScene()
+    {
+        MusicManager.Instance.StopLoungeMusic();
+        MusicManager.Instance.PlayInterviewMusic();
+        _fadeScreen.FadeOut(() =>
+        {
+            Camera.main.gameObject.SetActive(false);
+            Controller.Instance.enabled = false;
+
+            var async = SceneManager.LoadSceneAsync(Scenes.INTERVIEW_SCENE, LoadSceneMode.Additive);
             async.completed += OnSceneLoaded;
         });
     }
 
     private void OnSceneLoaded(AsyncOperation op)
     {
+        _fadeScreen.FadeIn2();
+    }
+
+    private void OnLoungeSceneLoaded(AsyncOperation op)
+    {
+        Camera.main.gameObject.SetActive(true);
+        Controller.Instance.enabled = true;
         _fadeScreen.FadeIn2();
     }
 }
