a55696d7126e86603e37c08d2aff601c4fd4c6dc
Letete98 <santiago@tollan.io>
2023-01-03 12:15:24 +0100

    Uncomment FlagManager

--- Assets/Scripts/Exploration/Flags/FlagManager.cs
+++ Assets/Scripts/Exploration/Flags/FlagManager.cs
@@ -10,61 +10,17 @@ namespace Booble.Flags
 {
 	public class FlagManager : Singleton<FlagManager>
 	{
-		// public static string DataDirectory { get { return "Data"; } }
-		// public static string FlagFilePath { get { return DataDirectory+"/flags.dat"; } }
-
 		[SerializeField] private bool _deleteSaveFileOnAwake;
-
-		// private FlagList _flags;
 
         private void Awake()
         {
 #if UNITY_EDITOR
-			// if (_deleteSaveFileOnAwake) DeleteSaveFile();
 			if (_deleteSaveFileOnAwake)
 			{
 				ResetFlags();
 			}
 #endif
-
-			// if(File.Exists(FlagFilePath))
-            // {
-			// 	LoadFlags();
-            // }
-			// else
-            // {
-			// 	Directory.CreateDirectory(DataDirectory);
-			// 	_flags = new FlagList();
-			// 	SaveFlags();
-            // }
         }
-
-        // private void LoadFlags()
-        // {
-		// 	using (FileStream fs = new FileStream(FlagFilePath, FileMode.Open))
-		// 	{
-		// 		BinaryFormatter bf = new BinaryFormatter();
-		// 		_flags = (FlagList)bf.Deserialize(fs);
-		// 	}
-		// }
-
-        // private void SaveFlags()
-        // {
-		// 	using (FileStream fs = new FileStream(FlagFilePath, FileMode.Create))
-		// 	{
-		// 		BinaryFormatter bf = new BinaryFormatter();
-		// 		bf.Serialize(fs, _flags);
-		// 	}
-		// }
-
-		// [ContextMenu("Delete flag.dat file")]
-		// public void DeleteSaveFile()
-        // {
-		// 	if(File.Exists(FlagFilePath))
-		// 	{
-		// 		File.Delete(FlagFilePath);
-		// 	}
-        // }
 
         public void ResetFlags()
         {
@@ -80,7 +36,6 @@ namespace Booble.Flags
 			}
 			
 			return false;
-			// return (_flags.Flags.Find(f => f.FlagReference == flagRef).FlagState) == Flag.State.True;
         }
 
 		public void SetFlag(Flag.Reference flagRef, bool toTrue = true)
@@ -93,23 +48,6 @@ namespace Booble.Flags
 			{
 				PlayerPrefs.SetInt(flagRef.ToString(), (int)Flag.State.False);
 			}
-			// Flag flag = _flags.Flags.Find(f => f.FlagReference == flagRef);
-			// if(toTrue)
-            // {
-			// 	if (flag.FlagState == Flag.State.False)
-            //     {
-			// 		flag.FlagState = Flag.State.True;
-			// 		SaveFlags();
-            //     }
-            // }
-			// else
-            // {
-			// 	if (flag.FlagState == Flag.State.True)
-			// 	{
-			// 		flag.FlagState = Flag.State.False;
-			// 		SaveFlags();
-			// 	}
-			// }
         }
 
 		public bool FlagsSatisfied(List<Flag.Reference> trueFlags, List<Flag.Reference> falseFlags)
@@ -121,7 +59,6 @@ namespace Booble.Flags
 				foreach (Flag.Reference reference in trueFlags)
 				{
 					result = result && GetFlag(reference);
-					// result = result && (_flags.Flags.Find(flag => flag.FlagReference == reference).FlagState == Flag.State.True);
 				}
 			}
 
@@ -130,7 +67,6 @@ namespace Booble.Flags
 				foreach (Flag.Reference reference in falseFlags)
 				{
 					result = result && !GetFlag(reference);
-					// result = result && (_flags.Flags.Find(flag => flag.FlagReference == reference).FlagState == Flag.State.False);
 				}
 			}
 
