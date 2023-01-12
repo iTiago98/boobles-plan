edb86fb2f13a3d3ddb1ae4068290c68e185fbc86
Letete98 <santiago@tollan.io>
2023-01-12 15:23:55 +0100

    Cuadrao negro y cambia de lao si se sale de la pantalla

--- Assets/Scripts/Exploration/UI/Cursor.cs
+++ Assets/Scripts/Exploration/UI/Cursor.cs
@@ -15,10 +15,12 @@ namespace Booble.UI
 		private static string _interactString = "Interactuar";
 
 		[SerializeField] private RectTransform _cursorT;
+		[SerializeField] private RectTransform _backgroundT;
+		[SerializeField] private Vector2 _defaultAnchoredPosition;
+		[SerializeField] private Vector2 _alternateAnchoredPosition;
 
 		private CanvasScaler _canvasScaler;
 		private TextMeshProUGUI _actionText;
-
 		private CursorText _currentCursorText = CursorText.None;
 
 		private void Awake()
@@ -26,7 +28,7 @@ namespace Booble.UI
 			UnityEngine.Cursor.lockState = CursorLockMode.Confined;
 			_canvasScaler = _cursorT.GetComponentInParent<CanvasScaler>();
 			_actionText = _cursorT.GetComponentInChildren<TextMeshProUGUI>();
-			_actionText.gameObject.SetActive(false);
+			_backgroundT.gameObject.SetActive(false);
 			SetApproachText();
 		}
 
@@ -37,6 +39,17 @@ namespace Booble.UI
 				Input.mousePosition.x.Map(0, Screen.width, -_canvasScaler.referenceResolution.x / 2, _canvasScaler.referenceResolution.x / 2),
 				Input.mousePosition.y.Map(0, Screen.height, -_canvasScaler.referenceResolution.y / 2, _canvasScaler.referenceResolution.y / 2)
 			);
+
+			float cursorWidth = _cursorT.sizeDelta.x.Map(0,
+				_canvasScaler.referenceResolution.x, 0, Screen.width);
+			float backgroundWidth = _backgroundT.sizeDelta.x.Map(0,
+				_canvasScaler.referenceResolution.x, 0, Screen.width);
+			float offset = _defaultAnchoredPosition.x.Map(0,
+				_canvasScaler.referenceResolution.x, 0, Screen.width);
+			float x = _cursorT.position.x + cursorWidth/2 + offset + backgroundWidth/2;
+			
+			_backgroundT.anchoredPosition = x < Screen.width ? _defaultAnchoredPosition : _alternateAnchoredPosition;
+			Debug.Log(x + " <> " + Screen.width);
 		}
 
 		public void SetApproachText()
@@ -59,7 +72,7 @@ namespace Booble.UI
 
 		public void ShowActionText(bool isShown)
         {
-			_actionText.gameObject.SetActive(isShown);
+	        _backgroundT.gameObject.SetActive(isShown);
         }
     }
 }
