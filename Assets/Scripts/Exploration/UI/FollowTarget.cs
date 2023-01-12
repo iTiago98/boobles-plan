febca07d423d6327a62a84cdccaba6638a114e6b
Letete98 <santiago@tollan.io>
2023-01-12 13:18:11 +0100

    Offset set to right of Nela in all scenes

--- Assets/Scripts/Exploration/UI/FollowTarget.cs
+++ Assets/Scripts/Exploration/UI/FollowTarget.cs
@@ -9,12 +9,11 @@ namespace Booble.UI
 	public class FollowTarget : MonoBehaviour
 	{
 		[SerializeField] private Transform _target;
+		[SerializeField] private Vector2 _offset;
 
         private Camera _cam;
         private RectTransform _rectTransform;
         private CanvasScaler _canvasScaler;
-
-		private Vector2 _offset;
 
         private void Awake()
         {
@@ -22,11 +21,11 @@ namespace Booble.UI
             _rectTransform = (RectTransform)transform;
             _canvasScaler = GetComponentInParent<CanvasScaler>();
 
-            Vector2 playerScreenPosition = _cam.WorldToScreenPoint(_target.position);
-            _offset = _rectTransform.anchoredPosition - new Vector2(
-                playerScreenPosition.x.Map(0, Screen.width, -_canvasScaler.referenceResolution.x / 2, _canvasScaler.referenceResolution.x / 2),
-                playerScreenPosition.y.Map(0, Screen.height, -_canvasScaler.referenceResolution.y / 2, _canvasScaler.referenceResolution.y / 2)
-            );
+            // Vector2 playerScreenPosition = _cam.WorldToScreenPoint(_target.position);
+            // _offset = _rectTransform.anchoredPosition - new Vector2(
+            //     playerScreenPosition.x.Map(0, Screen.width, -_canvasScaler.referenceResolution.x / 2, _canvasScaler.referenceResolution.x / 2),
+            //     playerScreenPosition.y.Map(0, Screen.height, -_canvasScaler.referenceResolution.y / 2, _canvasScaler.referenceResolution.y / 2)
+            // );
 
             gameObject.SetActive(false);
         }
@@ -38,6 +37,17 @@ namespace Booble.UI
             (
                 playerScreenPosition.x.Map(0, Screen.width, -_canvasScaler.referenceResolution.x / 2, _canvasScaler.referenceResolution.x / 2),
                 playerScreenPosition.y.Map(0, Screen.height, -_canvasScaler.referenceResolution.y / 2, _canvasScaler.referenceResolution.y / 2)
+            );
+        }
+
+        [ContextMenu("Set Offset")]
+        public void SetOffset()
+        {
+            CanvasScaler cs = GetComponentInParent<CanvasScaler>();
+            Vector2 playerScreenPosition = Camera.main.WorldToScreenPoint(_target.position);
+            _offset = ((RectTransform)transform).anchoredPosition - new Vector2(
+                playerScreenPosition.x.Map(0, Screen.width, -cs.referenceResolution.x / 2, cs.referenceResolution.x / 2),
+                playerScreenPosition.y.Map(0, Screen.height, -cs.referenceResolution.y / 2, cs.referenceResolution.y / 2)
             );
         }
     }
