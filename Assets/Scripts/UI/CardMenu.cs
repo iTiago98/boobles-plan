5293ba245816178818e0363d3a4472b5e5b1fe2c
Lluis Miralles <luismiralles98@gmail.com>
2023-01-12 12:34:50 +0100

    Fixed cards updated

--- Assets/Scripts/UI/CardMenu.cs
+++ Assets/Scripts/UI/CardMenu.cs
@@ -13,6 +13,7 @@ namespace Booble.UI
         [Header("Player")]
         [SerializeField] private GameObject _playerCardsMenu;
         [SerializeField] private GameObject _playerCardsList;
+        [SerializeField] private Button _playerButton;
         [SerializeField] private Sprite _playerCardBack;
 
         [Header("Citriano")]
@@ -46,29 +47,81 @@ namespace Booble.UI
 
         public void SetCardsMenu()
         {
-            if (!_initialized) SetPlayerCards();
-            //SetButtonsAvailable();
+            if (!_initialized) GetPlayerBaseCards();
+            UpdateExtraCards();
+            SetButtonsAvailable();
         }
 
-        private void SetPlayerCards()
+        private void GetPlayerBaseCards()
         {
             _playerBaseCards = DeckManager.Instance.GetPlayerBaseCards();
             _currentMenu = _playerCardsMenu;
             _initialized = true;
         }
 
-        private void SetButtonsAvailable()
+        private void UpdateExtraCards()
         {
-            _citrianoButton.interactable = FlagManager.Instance.GetFlag(Flag.Reference.Car1);
-            _ppBrosButton.interactable = FlagManager.Instance.GetFlag(Flag.Reference.Car2);
-            _secretaryButton.interactable = FlagManager.Instance.GetFlag(Flag.Reference.Car3);
-            _bossButton.interactable = FlagManager.Instance.GetFlag(Flag.Reference.Car3);
+            if (_currentMenu == _playerCardsMenu)
+            {
+                List<CardsData> temp = new List<CardsData>(_playerBaseCards);
+                temp.Add(DeckManager.Instance.GetGranFinal());
+                SetCards(_playerCardsList, temp, _playerCardBack);
+            }
+            else if (_currentMenu == _citrianoCardsMenu)
+            {
+                SetCards(_citrianoCardsList, DeckManager.Instance.GetCitrianoExtraCards(), _citrianoCardBack);
+            }
+            else if (_currentMenu == _ppBrosCardsMenu)
+            {
+                SetCards(_ppBrosCardsList, DeckManager.Instance.GetPPBrosExtraCards(), _ppBrosCardBack);
+            }
+            else if (_currentMenu == _secretaryCardsMenu)
+            {
+                SetCards(_secretaryCardsList, DeckManager.Instance.GetSecretaryExtraCards(), _secretaryCardBack);
+            }
+            else if (_currentMenu == _bossCardsMenu)
+            {
+                SetCards(_bossCardsList, DeckManager.Instance.GetBossExtraCards(), _bossCardBack);
+            }
         }
 
-        private void SetCards(GameObject parent, GameObject cardsParent, List<CardsData> cards, Sprite cardBack)
+        private void SetButtonsAvailable()
         {
-            if (parent != null) parent.SetActive(true);
+            bool flag0, flag1, flag2, flag3;
 
+            flag0 = FlagManager.Instance.GetFlag(Flag.Reference.Car0);
+            flag1 = FlagManager.Instance.GetFlag(Flag.Reference.Car1);
+            flag2 = FlagManager.Instance.GetFlag(Flag.Reference.Car2);
+            flag3 = FlagManager.Instance.GetFlag(Flag.Reference.Car3);
+
+            SetAvailable(_playerButton, flag0, GetInteractable(_playerCardsMenu));
+            SetAvailable(_citrianoButton, flag1, GetInteractable(_citrianoCardsMenu));
+            SetAvailable(_ppBrosButton, flag2, GetInteractable(_ppBrosCardsMenu));
+            SetAvailable(_secretaryButton, flag3, GetInteractable(_secretaryCardsMenu));
+            SetAvailable(_bossButton, flag3, GetInteractable(_bossCardsMenu));
+        }
+
+        private bool GetInteractable(GameObject menu)
+        {
+            return _currentMenu != menu;
+        }
+
+        private void SetAvailable(Button button, bool available, bool interactable = false)
+        {
+            if (available)
+            {
+                button.image.color = Color.white;
+                button.interactable = interactable;
+            }
+            else
+            {
+                button.image.color = Color.black;
+                button.interactable = false;
+            }
+        }
+
+        private void SetCards(GameObject cardsParent, List<CardsData> cards, Sprite cardBack)
+        {
             int index = 0;
             foreach (Transform cardObject in cardsParent.transform)
             {
@@ -90,53 +143,47 @@ namespace Booble.UI
             }
         }
 
+        #region Buttons Click
         public void OnPlayerButtonClick()
         {
             _currentMenu.SetActive(false);
             _playerCardsMenu.SetActive(true);
             _currentMenu = _playerCardsMenu;
+            UpdateExtraCards();
         }
 
         public void OnCitrianoButtonClick()
         {
             _currentMenu.SetActive(false);
             _citrianoCardsMenu.SetActive(true);
-
-            SetCards(_citrianoCardsMenu, _citrianoCardsList, DeckManager.Instance.GetCitrianoExtraCards(), _citrianoCardBack);
-
             _currentMenu = _citrianoCardsMenu;
+            UpdateExtraCards();
         }
-        
+
         public void OnPPBrosButtonClick()
         {
             _currentMenu.SetActive(false);
             _ppBrosCardsMenu.SetActive(true);
-
-            SetCards(_ppBrosCardsMenu, _ppBrosCardsList, DeckManager.Instance.GetPPBrosExtraCards(), _ppBrosCardBack);
-
             _currentMenu = _ppBrosCardsMenu;
+            UpdateExtraCards();
         }
-        
+
         public void OnSecretaryButtonClick()
         {
             _currentMenu.SetActive(false);
             _secretaryCardsMenu.SetActive(true);
-
-            SetCards(_secretaryCardsMenu, _secretaryCardsList, DeckManager.Instance.GetSecretaryExtraCards(), _secretaryCardBack);
-
             _currentMenu = _secretaryCardsMenu;
+            UpdateExtraCards();
         }
-        
+
         public void OnBossButtonClick()
         {
             _currentMenu.SetActive(false);
             _bossCardsMenu.SetActive(true);
-
-            SetCards(_bossCardsMenu, _bossCardsList, DeckManager.Instance.GetBossExtraCards(), _bossCardBack);
-
             _currentMenu = _bossCardsMenu;
+            UpdateExtraCards();
         }
 
-
+        #endregion
     }
 }
