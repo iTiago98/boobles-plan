05651b63ce3ddc8869f493c6ada04018bdbd6175
Letete98 <santiago@tollan.io>
2023-01-13 18:51:19 +0100

    Continuidad del arcadio

--- Assets/Interactables/Lounge0/Arcadio/StateCheck.cs
+++ Assets/Interactables/Lounge0/Arcadio/StateCheck.cs
@@ -8,6 +8,7 @@ namespace Booble.Interactables.Arcadio
 {
     public class StateCheck : MonoBehaviour
     {
+        [SerializeField] private Collider2D _arcadeCollider;
         [SerializeField] private Animator _anim;
         [SerializeField] private Flag.Reference _arcadioPartida;
         [SerializeField] private List<Flag.Reference> _coins;
@@ -17,14 +18,37 @@ namespace Booble.Interactables.Arcadio
             if(!FlagManager.Instance.GetFlag(_arcadioPartida))
                 return;
 
-            if (FlagManager.Instance.FlagsSatisfied(_coins, new List<Flag.Reference>()))
+            int cc = 0;
+            for (int i = 0; i < _coins.Count; i++)
             {
+                if (FlagManager.Instance.GetFlag(_coins[i]))
+                {
+                    cc++;
+                }
+            }
+
+            if (cc >= 3)
+            {
+                _arcadeCollider.enabled = true;
                 gameObject.SetActive(false);
+            }
+            else if (cc == 1)
+            {
+                _anim.SetTrigger("Lose");
             }
             else
             {
                 _anim.SetTrigger("Wait");
             }
+            //
+            // if (FlagManager.Instance.FlagsSatisfied(_coins, new List<Flag.Reference>()))
+            // {
+            //     gameObject.SetActive(false);
+            // }
+            // else
+            // {
+            //     _anim.SetTrigger("Wait");
+            // }
         }
     }
 }
